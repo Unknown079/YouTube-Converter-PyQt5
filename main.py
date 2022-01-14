0165e481ac59da00b584ebf71c43d33a3d7d0e40
NotMilk <81826946+Unknown079@users.noreply.github.com>
2022-01-14 14:49:27 +0000

    Update main.py

--- main.py
+++ main.py
@@ -18,19 +18,19 @@ class MP4(QMainWindow):
         self.link.move(240, 215)
         self.link.setFixedHeight(50)
         self.link.setFixedWidth(550)
-        self.link.setStyleSheet("background: #FFFAFA; border: 1px solid black; color: solid black; border-radius: 4px; font: 13pt")
+        self.link.setStyleSheet("background: #FFFAFA; border: 1px solid black; color: solid black; border-radius: 4px; font: 13pt;")
         self.link.setPlaceholderText("Insert a YouTube video URL here..")
 
         self.label = QLabel('Download MP4', self)
         self.label.move(410, 125)
-        self.label.setStyleSheet("color: solid black; font: 22pt")
+        self.label.setStyleSheet("color: solid black; font: 22pt;")
         self.label.adjustSize()
 
         self.button = QPushButton('Convert', self)
         self.button.move(430, 290)
         self.button.setFixedHeight(40)
         self.button.setFixedWidth(150)
-        self.button.setStyleSheet("background: #A52A2A; color: white; border: 1px solid black; border-radius: 4px; font: 10pt; font: 13pt")
+        self.button.setStyleSheet("background: #A52A2A; color: white; border: 1px solid black; border-radius: 4px; font: 10pt; font: 13pt; font-weight: bold;")
         self.button.clicked.connect(self.clicked)
 
     def clicked(self):
@@ -76,7 +76,7 @@ class MP3(QMainWindow):
         self.button.move(430, 290)
         self.button.setFixedHeight(40)
         self.button.setFixedWidth(150)
-        self.button.setStyleSheet("background: #A52A2A; color: white; border: 1px solid black; border-radius: 4px; font: 13pt")
+        self.button.setStyleSheet("background: #A52A2A; color: white; border: 1px solid black; border-radius: 4px; font: 13pt; font-weight: bold;")
         self.button.clicked.connect(self.clicked)
     
     def clicked(self):
@@ -128,12 +128,20 @@ def process():
     win1 = MP4()
     win2 = MP3()
     
-    x = pg.prompt("Choose how you want to convert: (MP3/MP4)\n", "YouTube Converter").upper()
-    if x == 'mp4'.upper():
-        win1.show()
-    elif x == 'mp3'.upper():
-        win2.show()
-
+    while True: 
+        x = pg.prompt("Choose how you want to convert: (MP3/MP4)\n", "YouTube Converter").upper()
+        if x == 'mp4'.upper():
+            win1.show()
+            break
+        elif x == 'mp3'.upper():
+            win2.show()
+            break
+        elif x != 'mp3'.upper():
+            pg.alert("Try again :)", "Error 404")
+            x = pg.prompt("Choose how you want to convert: (MP3/MP4)\n", "YouTube Converter").upper()
+        elif x != 'mp4'.upper():
+            pg.alert("Try again :)", "Error 404")
+            x = pg.prompt("Choose how you want to convert: (MP3/MP4)\n", "YouTube Converter").upper()
     app.setStyle("Fusion")
     app.exec_()
 process()
