8aaf3a3e2d4e1718c93c6d479f98cc50726db3d7
NotMilk <81826946+Unknown079@users.noreply.github.com>
2023-11-22 00:08:24 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from PyQt5.QtWidgets import *
 from PyQt5.QtGui import QIcon
-from youtube_dl import YoutubeDL
+from yt_dlp import YoutubeDL
 import os
 import pyautogui as pg
 
@@ -9,7 +9,7 @@ class MP4(QMainWindow):
         super(MP4, self).__init__()
         self.setGeometry(500, 200, 1000, 600)
         self.setWindowTitle("YouTube Converter")
-        self.setWindowIcon(QIcon('youtube.png'))
+        self.setWindowIcon(QIcon('youtube.ico'))
         self.setStyleSheet('background: #F0F8FF')
         self.UI()
     
@@ -18,44 +18,72 @@ class MP4(QMainWindow):
         self.link.move(240, 215)
         self.link.setFixedHeight(50)
         self.link.setFixedWidth(550)
-        self.link.setStyleSheet("background: #FFFAFA; border: 1px solid black; color: solid black; border-radius: 4px; font: 13pt;")
+        self.link.setStyleSheet("background: #FFFAFA; border: 1px solid black; color: solid black; border-radius: 20px; font: 13pt")
         self.link.setPlaceholderText("Insert a YouTube video URL here..")
 
         self.label = QLabel('Download MP4', self)
-        self.label.move(410, 125)
-        self.label.setStyleSheet("color: solid black; font: 22pt;")
+        self.label.move(400, 125)
+        self.label.setStyleSheet("color: solid black; font: 22pt; font-weight: bold;")
         self.label.adjustSize()
 
-        self.button = QPushButton('Convert', self)
-        self.button.move(430, 290)
+        self.button = QPushButton('CONVERT', self)
+        self.button.move(420, 315)
         self.button.setFixedHeight(40)
-        self.button.setFixedWidth(150)
-        self.button.setStyleSheet("background: #A52A2A; color: white; border: 1px solid black; border-radius: 4px; font: 10pt; font: 13pt; font-weight: bold;")
-        self.button.clicked.connect(self.clicked)
+        self.button.setFixedWidth(170)
+        self.button.setStyleSheet("background: #A52A2A; color: white; border: 1px solid black; border-radius: 20px; font: 13pt; font-weight: bold;")
+        self.button.clicked.connect(self.clicked2)
+
+        self.button2 = QPushButton('BACK', self)
+        self.button2.move(840, 530)
+        self.button2.setFixedHeight(40)
+        self.button2.setFixedWidth(100)
+        self.button2.setStyleSheet("background: #A52A2A; color: white; border: 1px solid black; border-radius: 20px; font: 13pt; font-weight: bold;")
+        self.button2.clicked.connect(self.clicked)
 
     def clicked(self):
+        self.win1 = MP4()
+        self.win2 = MP3()
+        
+        self.x = pg.prompt("Choose the format path: (MP3/MP4)\n", "YouTube Converter").upper()
+
+        while True:
+            if self.x == 'mp4'.upper():
+                self.win1.show()
+                break
+            elif self.x == 'mp3'.upper():
+                self.win2.show()
+                break
+            elif self.x != 'mp4'.upper():
+                pg.alert("Try again..", "Error 404")
+                self.x = pg.prompt("Choose the format path: (MP3/MP4)\n", "YouTube Converter").upper()
+            elif self.x != 'mp3'.upper():
+                pg.alert("Try again..", "Error 404")
+                self.x = pg.prompt("Choose the format path: (MP3/MP4)\n", "YouTube Converter").upper()
+        self.close()
+    
+    def clicked2(self):
         try:
             mp4_path = os.path.join(os.environ['USERPROFILE'],"Desktop")
             os.chdir(f"{mp4_path}")
             os.mkdir("MP4 Files")
             os.chdir(f'{mp4_path}\MP4 Files')
             
-            self.video_downloder = YoutubeDL({'format':'mp4'})
+            video_downloder = YoutubeDL({'format':'mp4'})
             new = self.link.text()
-            self.video_downloder.extract_info(new)
+            video_downloder.extract_info(new)
         
         except FileExistsError:
             os.chdir(f'{mp4_path}\MP4 Files')
-            self.video_downloder = YoutubeDL({'format':'mp4'})
+            video_downloder = YoutubeDL({'format':'mp4'})
             new = self.link.text()
-            self.video_downloder.extract_info(new)
+            video_downloder.extract_info(new)
         
 class MP3(QMainWindow):
     def __init__(self):
         super(MP3, self).__init__()
         self.setGeometry(500, 200, 1000, 600)
         self.setWindowTitle("YouTube Converter")
-        self.setWindowIcon(QIcon('youtube.png'))
+        self.setWindowIcon(QIcon('youtube.ico'))
         self.setStyleSheet('background: #F0F8FF')
         self.UI()
     
@@ -64,22 +92,50 @@ class MP3(QMainWindow):
         self.link.move(240, 215)
         self.link.setFixedHeight(50)
         self.link.setFixedWidth(550)
-        self.link.setStyleSheet("background: #FFFAFA; border: 1px solid black; color: solid black; border-radius: 4px; font: 13pt")
+        self.link.setStyleSheet("background: #FFFAFA; border: 1px solid black; color: solid black; border-radius: 20px; font: 13pt;")
         self.link.setPlaceholderText("Insert a YouTube video URL here..")
 
         self.label = QLabel('Download MP3', self)
-        self.label.move(410, 125)
-        self.label.setStyleSheet("color: solid black; font: 22pt")
+        self.label.move(400, 125)
+        self.label.setStyleSheet("color: solid black; font: 22pt; font-weight: bold;")
         self.label.adjustSize()
 
-        self.button = QPushButton('Convert', self)
-        self.button.move(430, 290)
+        self.button = QPushButton('CONVERT', self)
+        self.button.move(420, 315)
         self.button.setFixedHeight(40)
-        self.button.setFixedWidth(150)
-        self.button.setStyleSheet("background: #A52A2A; color: white; border: 1px solid black; border-radius: 4px; font: 13pt; font-weight: bold;")
-        self.button.clicked.connect(self.clicked)
-    
+        self.button.setFixedWidth(170)
+        self.button.setStyleSheet("background: #A52A2A; color: white; border: 1px solid black; border-radius: 20px; font: 13pt; font-weight: bold;")
+        self.button.clicked.connect(self.clicked2)
+
+        self.button2 = QPushButton('BACK', self)
+        self.button2.move(840, 530)
+        self.button2.setFixedHeight(40)
+        self.button2.setFixedWidth(100)
+        self.button2.setStyleSheet("background: #A52A2A; color: white; border: 1px solid black; border-radius: 20px; font: 13pt; font-weight: bold;")
+        self.button2.clicked.connect(self.clicked)
+        
     def clicked(self):
+        self.win1 = MP4()
+        self.win2 = MP3()
+        
+        self.x = pg.prompt("Choose the format path: (MP3/MP4)\n", "YouTube Converter").upper()
+
+        while True:
+            if self.x == 'mp4'.upper():
+                self.win1.show()
+                break
+            elif self.x == 'mp3'.upper():
+                self.win2.show()
+                break
+            elif self.x != 'mp4'.upper():
+                pg.alert("Try again..", "Error 404")
+                self.x = pg.prompt("Choose the format path: (MP3/MP4)\n", "YouTube Converter").upper()
+            elif self.x != 'mp3'.upper():
+                pg.alert("Try again..", "Error 404")
+                self.x = pg.prompt("Choose the format path: (MP3/MP4)\n", "YouTube Converter").upper()
+        self.close()
+
+    def clicked2(self):
         try:
             mp3_path = os.path.join(os.environ['USERPROFILE'], "Desktop")
             os.chdir(f'{mp3_path}')
@@ -128,20 +184,22 @@ def process():
     win1 = MP4()
     win2 = MP3()
     
-    while True: 
-        x = pg.prompt("Choose how you want to convert: (MP3/MP4)\n", "YouTube Converter").upper()
+    x = pg.prompt("Choose the format path: (MP3/MP4)\n", "YouTube Converter").upper()
+    
+    while True:
         if x == 'mp4'.upper():
             win1.show()
             break
         elif x == 'mp3'.upper():
             win2.show()
             break
-        elif x != 'mp3'.upper():
-            pg.alert("Try again :)", "Error 404")
-            x = pg.prompt("Choose how you want to convert: (MP3/MP4)\n", "YouTube Converter").upper()
         elif x != 'mp4'.upper():
-            pg.alert("Try again :)", "Error 404")
-            x = pg.prompt("Choose how you want to convert: (MP3/MP4)\n", "YouTube Converter").upper()
+            pg.alert("Try again..", "Error 404")
+            x = pg.prompt("Choose the format path: (MP3/MP4)\n", "YouTube Converter").upper()
+        elif x != 'mp3'.upper():
+            pg.alert("Try again..", "Error 404")
+            x = pg.prompt("Choose the format path: (MP3/MP4)\n", "YouTube Converter").upper()
+    
     app.setStyle("Fusion")
     app.exec_()
 process()
